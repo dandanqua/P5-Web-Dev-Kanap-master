5a8a234fb82552809235b70ef91e4f9858c8ad12
dandanqua <100298876+dandanqua@users.noreply.github.com>
2022-10-04 00:18:18 +0100

    2nd commit

---
+++ front/js/confirmation.js
@@ -0,0 +1,3 @@
+function checkout(){
+    const orderId = document
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,27 @@
 // constant for database URL
 const URL = "http://localhost:3000/api/products"
-//Calling fetch on that URL. This will instantly return a promise (if URL is valid databse).
-fetch( URL )
-    // the promise's `then()` handler is called with the response.
-    .then(response => { const json =response.json()
-        return json;
-    })
-    .then(data => {
-        displayproduct(data)
-    })
-.catch(_error => {
-    alert(console.error();
-});
-
-function displayproduct(data) {
-    for (product of data) {}
-}
+// Call `fetch()`, passing in the URL.
+fetch(URL)
+  // fetch() returns a promise. When we have received a response from the server,
+  // the promise's `then()` handler is called with the response.
+  .then((response) => {
+    // Our handler throws an error if the request did not succeed.
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    // Otherwise (if the response succeeded), our handler fetches the response
+    // as text by calling response.text(), and immediately returns the promise
+    // returned by `response.text()`.
+    return response.json();
+  })
+  // When response.text() has succeeded, the `then()` handler is called with
+  // the text, and we copy it into the `poemDisplay` box.
+  .then((data) => console.log(data))
+  // Catch any errors that might happen, and display a message
+  // in the `poemDisplay` box.
+  .catch((error) => {
+    console.log(error)
+    let sect
+    //poemDisplay.textContent = `Could not fetch verse: ${error}`
+  }
+);
